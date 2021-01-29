24e7d613fd94298f29d70df53b47fa2e708a2edd
matteobuscicchio <matteoezio@me.com>
2021-01-29 18:34:04 +0100

    inizio scrittura form con php

--- index.php
+++ index.php
@@ -48,14 +48,22 @@
 
         <!--
             PHP Snack 2:
-            Passare come parametri GET name, mail e age e verificare (cercando i metodi che non
-            conosciamo nella documentazione) che:
+            Passare come parametri GET name, mail e age e verificare 
+            (cercando i metodi che non conosciamo nella documentazione) che:
             1. name sia più lungo di 3 caratteri,
             2. mail contenga un punto e una chiocciola
             3. age sia un numero.
             Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
         -->
 
-        <?php ?>
+        <?php
+            $form = [
+                "name" => $_GET['name'],
+                "mail" => $_GET['mail'],
+                "age" => $_GET['age'],
+            ];
+
+            var_dump($form);
+        ?>
     </body>
 </html>
